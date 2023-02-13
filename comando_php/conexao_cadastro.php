1f6168b32548e7ea7cb556885edce7caf556a840
Antonio Gomes <likeartte@gmail.com>
2023-02-12 23:27:22 -0300

    conexao-db-tabela-12-02-20-23
    Estou iniciando a troca de uma tabela simples por uma completa de cadastro de muito dados em varias tabelas do bd. ainda vou mexer no corpo para validar.

---
+++ comando_php/conexao_cadastro.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "acgsB07";
+$dbname = "db_tcc_estacionamento";
+$port = 3306;
+
+try{
+    //Conexão com a porta
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+
+    //Conexão sem a porta
+    //$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+
+    //echo "Conexão com banco de dados realizado com sucesso!";
+}  catch(PDOException $err){
+    echo "Erro: Conexão com banco de dados não foi realizada com sucesso. Erro gerado " . $err->getMessage();
+}
+
+
