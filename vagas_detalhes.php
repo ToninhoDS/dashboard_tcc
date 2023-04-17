8c62d6e3af2e290ab4e9dccc8c056f5c90c079ad
Antonio Gomes <likeartte@gmail.com>
2023-04-17 01:44:01 -0300

    Revert "DASCHBOARD PRONTA 03 - REGRAS DE NEGOCIO"
    This reverts commit b9f57aa727814f24c0efd8e96b9080b6d1ac360c.

--- vagas_detalhes.php
+++ vagas_detalhes.php
@@ -345,9 +345,9 @@ include("comando_php/crud_php/conexao_cadastro.php");
                         </div>
                         <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
                             <div class="text-md-right footer-links d-none d-sm-block">
-                                <a href="javascript: void(0);">Voltar</a>
-                                <a href="javascript: void(0);">Suporte</a>
-                                <a href="javascript: void(0);">ContatoUs</a>
+                                <a href="javascript: void(0);">About</a>
+                                <a href="javascript: void(0);">Support</a>
+                                <a href="javascript: void(0);">Contact Us</a>
                             </div>
                         </div>
                     </div>
